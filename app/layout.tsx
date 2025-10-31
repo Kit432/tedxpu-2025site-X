fa98b6cdc63c80707ba1c0937e013df1556bb5a4
Flamekit <kitlin093@gmail.com>
2025-10-31 18:37:24 +0200

    Mobile change

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "TEDxPanteionUniversity",
   description: "TEDxPU 2026 website",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
