9657bfa17ff4e2404d4a967d5ed283c8257f8d48
Kit <linhongkit@gmail.com>
2026-05-19 18:35:56 +0300

    added signature

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 /* Homepage */
-import Image from "next/image";
 import HeroSection from "@/components/home/HeroSection";
-import Marquee from "@/components/home/Marquee";
 import Timeline from "@/components/home/Timeline";
+import ConsoleSignature from "../components/layout/ConsoleSignature";
 
 export default function Home() {
   return (
     <main className="min-h-screen text-white py-16 md:py-0">
       <section style={{}}>
+        {/*
+          Built by Λιν Χονγκ Τσε (Κιτ) — TEDxPanteion University X Website
+          Github: https://github.com/Kit432
+      */}
+        <ConsoleSignature />
         <HeroSection />
       </section>
         <Timeline />
